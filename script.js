d28e79b25fc08b38cff8a59c0fd6971990be82e4
William Gonzales <wgonz94@gmail.com>
2022-02-05 14:13:42 -0800

    added hero ability and powers

---
+++ script.js
@@ -0,0 +1,6 @@
+//an array of random hero's ability/powers/role
+const heroPower = ["Sun Breathe Sword Style", "Lightning Breathe Sword Style", "Beast Breathe Sword Style", "One-hit Punch", "Cyborg Martial Artist", "Soul Reaper", "Ultra Instinct", "G.O.D.", "One-for-All Quirk", "Explosion Quirk", "Fire and Ice Quirk", "Spider-based power", "Super-human enhanced power", "Genius-level intellect/ expert detective skilled martial artist", "Ring based on imagination/courage", "Super Speed", "Gamma-radiation", "magical hammer", "mechanical suit", "magical sword", "master assasin", "master archer", "master wizard"];
+//an array of random hero's names
+const heroName = ["Tanjiro Kamado - DemonSlayer", ""];
+//an array of catchphrases
+const heroPhrase = [""];
